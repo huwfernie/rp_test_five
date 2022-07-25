84422e921535b386bb4cde55aec2e9f1e530b708
huwfernie <huwfernie@gmail.com>
2022-07-25 12:13:31 +0100

    test

--- server2.js
+++ server2.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 //Static Routes
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 //Main App Route
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
