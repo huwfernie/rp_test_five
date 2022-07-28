ef6852140b9b7f3b5e458d0e1e64ce889b6b8b0c
huwfernie <huwfernie@gmail.com>
2022-07-28 15:08:26 +0100

    test

--- server2.js
+++ server2.js
@@ -33,7 +33,7 @@ app.listen(port, async () => {
 // const command = 'ffmpeg -ar 8000 -t 60 -ac 1 -f alsa -i hw:1,0 -ac 1 -acodec mp2 -b:a 128k -f rtp rtp://192.168.1.173:4001'
 // commandLine(command);
 
-const proc = require('child_process').spawn('sudo /home/pi/darkice.sh');
+const proc = require('child_process').spawn('/home/pi/darkice.sh');
 process.on('SIGINT', () => {
   console.log('Stopping darkice');
   proc.kill('SIGINT');
