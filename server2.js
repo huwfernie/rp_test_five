73eecd5e26ee9d35a402464419bd34668c1deb2e
huwfernie <huwfernie@gmail.com>
2022-07-28 15:02:47 +0100

    test

--- server2.js
+++ server2.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Helpers
 const whatsMyIp = require('./helpers/whats-my-ip');
-const commandLine = require('./helpers/command-line');
+// const commandLine = require('./helpers/command-line');
 
 //Static Routes
 app.use('/', express.static(path.join(__dirname, '/public')));
@@ -30,5 +30,12 @@ app.listen(port, async () => {
   console.log(`Try :: open http://${ip}:${port}/`);
 });
 
-const command = 'ffmpeg -ar 8000 -t 60 -ac 1 -f alsa -i hw:1,0 -ac 1 -acodec mp2 -b:a 128k -f rtp rtp://192.168.1.173:4001'
-commandLine(command);
+// const command = 'ffmpeg -ar 8000 -t 60 -ac 1 -f alsa -i hw:1,0 -ac 1 -acodec mp2 -b:a 128k -f rtp rtp://192.168.1.173:4001'
+// commandLine(command);
+
+const proc = require('child_process').spawn('sudo /home/pi/darkice.sh');
+process.on('SIGINT', () => {
+  console.log('Stopping darkice');
+  proc.kill('SIGINT');
+  console.log('Stopped darkice');
+});
